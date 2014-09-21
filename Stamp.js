7578a47c8a0b50c42b32895f0c129429c65ab46c
Scott Byrns <contact@scottbyrns.com>
2014-09-21 00:33:20 -0600

    Improved performance

--- Stamp.js
+++ Stamp.js
@@ -1,15 +1,20 @@
 
 // Stamp - 1.0.0-SNAPSHOT
 (function () {
-	var stamp = function (elementName, template) {
+	var stamp = function (template) {
 		var elementTemplate = template.import;
+		var templateNode = elementTemplate.getElementsByTagName("template")[0];
+		var elementName = templateNode.getAttribute("element");
+		var constructor = templateNode.getAttribute("constructor");
 		var proto = Object.create(HTMLElement.prototype, {
 			createdCallback: { value: function() {
-				var clone = document.importNode(elementTemplate.getElementsByTagName("template")[0].content, true);
+				var clone = document.importNode(templateNode.content, true);
 				this.createShadowRoot().appendChild(clone);
-				var constructor = elementTemplate.getElementsByTagName("template")[0].getAttribute("constructor");
 				if (constructor) {
-					window[constructor].call(this);
+					try {
+						window[constructor].call(this);
+					}
+					catch (e) {}
 				}
 			}}
 		});
@@ -19,7 +24,7 @@
 	var links = document.getElementsByTagName("link");
 	for (var i = 0, len = links.length; i < len; i += 1) {
 		if (links[i].getAttribute("rel") == "import") {
-			stamp(links[i].getAttribute("id"), links[i])
+			stamp(links[i])
 		}
 	}
 }());
